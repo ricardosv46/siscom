9ea4f95bdd04eec44e69701963f6e21ad0b75902
Cristhian R. Alarcon Aguilar <CAlarcon@onpe.gob.pe>
2023-06-08 08:28:00 -0500

    Updates

--- pages/listadopas.tsx
+++ pages/listadopas.tsx
@@ -524,25 +524,17 @@ const Listadopas: NextPageWithLayout<ListadopasProps> = ({
                 {filesContent.length == 1 && processFile(plainFiles[0])}
               </div>
               <div style={{display: 'flex', alignItems: 'center'}}>
-
-                {(process !== null) && (<Button style={{height:'50px', color:'white', backgroundColor:'#083474', cursor:'pointer',fontSize:'1rem', marginRight: '5px'}} onClick={() => ExportExcel(inputValue ? filterData : process)}>Reporte PAS</Button>)}
-                {(process === null) && (<Button style={{height:'50px', color:'white', backgroundColor:'#083474', cursor:'pointer',fontSize:'1rem', marginRight: '5px'}} onClick={() => alert('No hay datos para descargar')}>Reporte PAS</Button>)}
-              </div>
-              <div style={{display: 'flex', alignItems: 'center'}}>
-                {(process !== null) && (<Button style={{height:'50px', color:'white', backgroundColor:'#0874cc', cursor:'pointer',fontSize:'1rem', marginRight: '5px'}} onClick={() => DescargarExcel()}>Detalle</Button>)}
-                {(process === null) && (<Button style={{height:'50px', color:'white', backgroundColor:'#0874cc', cursor:'pointer',fontSize:'1rem', marginRight: '5px'}} onClick={() => alert('No hay datos para descargar')}>Detalle</Button>)}
-=======
-                <Button style={{display:'flex', alignItems:'center',
+                <Button hidden={!process} style={{display:'flex', alignItems:'center',
                  justifyContent:'center', padding:'8px 8px',
                  backgroundColor:'#083474', border:'none',
                  color:'white', marginRight: '10px', cursor:'pointer'}} 
                  onClick={() => ExportExcel(inputValue ? filterData : process)}>
                   <img src='assets/images/reporte_pas.svg' style={{width: '24px', height: '24px', marginRight: '8px'}}/>
                   <span style={{fontSize: '16px'}}>Reporte PAS</span>
-                  </Button>
+                </Button>
               </div>
               <div style={{display: 'flex', alignItems: 'center'}}>
-                <Button style={{display:'flex', alignItems:'center',
+              <Button hidden={!process} style={{display:'flex', alignItems:'center',
                  justifyContent:'center', padding:'8px 8px',
                  backgroundColor:'#0874cc', border:'none',
                  color:'white', marginRight: '10px', cursor:'pointer'}} 
@@ -550,7 +542,6 @@ const Listadopas: NextPageWithLayout<ListadopasProps> = ({
                   <img src='assets/images/icono_detalle.svg' style={{width: '24px', height: '24px', marginRight: '8px'}}/>
                   <span style={{fontSize: '16px'}}>Detalle</span>
                   </Button>
-
               </div>
             </div>
         </div>
