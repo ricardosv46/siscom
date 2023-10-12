eca78759b1a4daf880ebdee232b3da7d4bb4fcc2
Ricardo M. Solis Vizurraga <RSolis@onpe.gob.pe>
2023-10-12 12:17:17 -0500

    vista estadistica terminada

--- components/ui/Card/Card.tsx
+++ components/ui/Card/Card.tsx
@@ -1,12 +1,12 @@
-import { FC, ReactNode } from "react";
+import { FC, HtmlHTMLAttributes, ReactNode } from "react";
 import s from "./Card.module.css";
 
-interface CardProps {
+interface CardProps extends HtmlHTMLAttributes<HTMLDivElement> {
   title?: string;
   children: ReactNode;
   border?: boolean;
 }
-const Card: FC<CardProps> = ({ title, children, border = true }) => {
-  return <div className={border ? s.root : s.root__within__border}>{children}</div>;
+const Card: FC<CardProps> = ({ title, children, border = true, className, ...props }) => {
+  return <div className={`${border ? s.root : ""} ${className}`}>{children}</div>;
 };
 export default Card;
